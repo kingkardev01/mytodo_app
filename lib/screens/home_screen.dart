d32c6cdce6b8f9fd5b8469c15aae1d78d02e3b10
kingkar <kingkarbiswas001@gmail.com>
2023-04-06 20:51:23 +0600

    home screen add

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("To-Do List"),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.calendar_view_day),
+            ),
+          ],
+        )
+    );
+  }
+}
